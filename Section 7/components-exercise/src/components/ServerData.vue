a2eb96490ee55cfcf54ec6647817df956e96a232
Yuliyan Ivanov <winsomewe.kek37@gmail.com>
2020-08-08 18:51:50 +0300

    Component exercise created

---
+++ Section 7/components-exercise/src/components/ServerData.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12 col-sm-6">
+      <ul class="list-group">
+        <li class="list-group-item" v-for="(index, idx) in 5" :key="idx">Server #{{ index }}</li>
+      </ul>
+    </div>
+    <div class="col-xs-12 col-sm-6">
+      <p>Server Details are currently not updated</p>
+    </div>
+  </div>
+</template>
