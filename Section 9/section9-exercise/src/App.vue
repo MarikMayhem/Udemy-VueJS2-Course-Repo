cefdceb7dfba95bcfa7e8047e25b1467e13a483f
Yuliyan Ivanov <winsomewe.kek37@gmail.com>
2020-08-10 16:25:52 +0300

    Section 9, exercise created

---
+++ Section 9/section9-exercise/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <br />
+        <button @click="changeComponentStatus($event)" class="btn btn-primary">Load Blue Template</button>
+        <button @click="changeComponentStatus($event)" class="btn btn-success">Load Green Template</button>
+        <button @click="changeComponentStatus($event)" class="btn btn-danger">Load Red Template</button>
+        <hr />component
+        <component :is="currentComponent">
+          <h1>I am a {{currentComponent}} component</h1>
+          <p>A great component</p>
+          <p slot="badComponent">But sometimes a very bad Component</p>
+        </component>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Blue from "./components/Blue.vue";
+import Green from "./components/Green.vue";
+import Red from "./components/Red.vue";
+
+export default {
+  data() {
+    return {
+      currentComponent: "Blue",
+    };
+  },
+  components: {
+    Blue,
+    Green,
+    Red,
+  },
+  methods: {
+    changeComponentStatus(status) {
+      let componentStatus = status.srcElement.innerText.split(" ")[1];
+      this.currentComponent = componentStatus;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ Section 9/section9-exercise/src/components/Red.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <slot></slot>
+    <slot name="badComponent"></slot>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+div {
+  border: 1px solid red;
+  background-color: lightcoral;
+  padding: 30px;
+  margin: 20px auto;
+  text-align: center;
+}
+</style>
